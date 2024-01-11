921411a63966cd6dde34696a36284c132a5b1a3e
Saubhagya Gupta <saubhagya18gupta@gmail.com>
2024-01-11 14:31:57 +0530

    Commit to lay the foundation of project structure

---
+++ Magic_Arena_App.java
@@ -0,0 +1,8 @@
+import models.Magic_Arena_Game;
+
+public class Magic_Arena_App {
+
+    public static void main(String[] args) {
+        Magic_Arena_Game newGame = new Magic_Arena_Game();
+    }
+}
---
+++ models/Magic_Arena_Game.java
@@ -0,0 +1,5 @@
+package models;
+
+public class Magic_Arena_Game {
+    
+}
