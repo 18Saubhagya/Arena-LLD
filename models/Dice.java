2914074e202a3437ea54666f724015df38a5e443
Saubhagya Gupta <saubhagya18gupta@gmail.com>
2024-01-11 14:56:50 +0530

    Develop Dice Class

--- models/Dice.java
+++ models/Dice.java
@@ -1,5 +1,29 @@
 package models;
 
 public class Dice {
-    
+    private int diceCount;
+
+    public Dice() {
+        diceCount=1;
+    }
+    public Dice(int count) {
+        diceCount=count;
+    }
+    public int getDiceCount() {
+        return diceCount;
+    }
+    public void setDiceCount(int count) {
+        diceCount=count;
+    }
+
+    public int rollDice() {
+        int sum=0,currentCount=diceCount;
+        while(currentCount>0)
+        {
+            int diceNum=(int)Math.random()*6+1;
+            sum+=diceNum;
+            --currentCount;
+        }
+        return sum;
+    }
 }
